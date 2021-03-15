efabc1118cc0c8030b4ad615120e572f0a8f7665
werlandy <422606101@qq.com>
2021-03-16 00:14:27 +0800

    update uniswap

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.97786670d890f1e326b19695dfc9ab61.js"
+  "./precache-manifest.fd2ffa7288a443ff54b3212aa457b72c.js"
 );
 
 self.addEventListener('message', (event) => {
